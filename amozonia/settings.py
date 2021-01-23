b8bff3185cecad0fb942f03addd1a4beaf49e2b4
manalone <depotter.laurens@gmail.com>
2021-01-22 23:46:56 -0300

    Added cart functions

--- amozonia/settings.py
+++ amozonia/settings.py
@@ -60,7 +60,11 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static_files'),
+    os.path.join(BASE_DIR, 'node_modules', 'd3', 'build'),
+    os.path.join(BASE_DIR, 'node_modules', 'c3'),
+    )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- core/views/cart.py
+++ core/views/cart.py
@@ -35,17 +35,41 @@ def add_to_cart(request, slug):
             order_item.quantity += 1
             order_item.save()
             messages.info(request, "Este item foi atualizado no seu carrinho.")
-            return redirect("core:product", slug=slug)
+            return redirect("core:cart")
         else:
             messages.info(request, "Este item foi adicionado ao seu carrinho.")
             order.items.add(order_item)
-            return redirect("core:product", slug=slug)
+            return redirect("core:cart")
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
         messages.info(request, "Este item foi adicionado ao seu carrinho.")
-        return redirect("core:product", slug=slug)
+        return redirect("core:cart")
+
+# @login_required
+# def add_to_cart(request, slug):
+#     item = get_object_or_404(Item, slug=slug)
+#     order_item, created = OrderItem.objects.get_or_create(item=item, user=request.user, ordered=False)
+#     order_qs = Order.objects.filter(user=request.user, ordered=False)
+#     if order_qs.exists():
+#         order = order_qs[0]
+#         # check if the order item is in the order
+#         if order.items.filter(item__slug=item.slug).exists():
+#             order_item.quantity += 1
+#             order_item.save()
+#             messages.info(request, "Este item foi atualizado no seu carrinho.")
+#             return redirect("core:product", slug=slug)
+#         else:
+#             messages.info(request, "Este item foi adicionado ao seu carrinho.")
+#             order.items.add(order_item)
+#             return redirect("core:product", slug=slug)
+#     else:
+#         ordered_date = timezone.now()
+#         order = Order.objects.create(user=request.user, ordered_date=ordered_date)
+#         order.items.add(order_item)
+#         messages.info(request, "Este item foi adicionado ao seu carrinho.")
+#         return redirect("core:product", slug=slug)
 
 @login_required
 def remove_from_cart(request, slug):
